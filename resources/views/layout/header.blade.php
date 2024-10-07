984a61056773393574e339ea68bf69dab6031997
wildan <wildantaw@gmail.com>
2024-10-07 22:03:09 +0700

    indexterbaru

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,30 +1,31 @@
-<!-- Start Header/Navigation -->
-<nav class="custom-navbar navbar navbar-expand-md navbar-dark bg-biru-gelap shadow" aria-label="Furni navigation bar" style="background: linear-gradient(to right, #2eaf88 , #0dbde6);">
 
-	<div class="container">
-		<img src="{{asset('images/logorev.png')}}" style="width: 5%; height: auto;" class="">
-		<a class="navbar-brand" href="index.html">Pos<span>UMKM</span></a>
+	<!-- Start Header/Navigation -->
+	<nav class="custom-navbar navbar navbar-expand-md navbar-dark bg-biru-gelap shadow" aria-label="Furni navigation bar">
 
-		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni" aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
+		<div class="container">
+			<img src="{{asset('images/POSUMKMbg.png')}}" style="width: 5%; height: auto;" class="">
+			<a class="navbar-brand" href="index.html">Pos<span>UMKM</span></a>
 
-		<div class="collapse navbar-collapse" id="navbarsFurni " style="">
-			<ul class="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
-				<li class="nav-item active">
-				<li><a class="nav-link" href="{{ url('/') }}" style="color:black !important">Beranda</a></li>
-				<li><a class="nav-link" href="{{ url('/event') }}" style="color:black !important">Event</a></li>
-				<li><a class="nav-link" href="about.html" style="color:black !important">UMKM</a></li>
-				<li><a class="nav-link" href="about.html" style="color:black !important">Konsultasi UMKM</a></li>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni" aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
 
+			<div class="collapse navbar-collapse" id="navbarsFurni">
+				<ul class="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
+					<li class="nav-item active">
+						<a class="nav-link" href="index.html">Beranda</a>
+					</li>
+					<li><a class="nav-link" href="zz.html">Event</a></li>
+					<li><a class="nav-link" href="about.html">UMKM</a></li>
+					<li><a class="nav-link" href="about.html">Konsultasi UMKM</a></li>
 
-			</ul>
+				</ul>
 
-			<ul class="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
-				<li><a class="nav-link" href="#"><img src="{{asset('images/user.svg')}}"></a></li>
-				<li><a class="btn btn-secondary me-2" href="{{ route('login') }}">Login</a></li>
-			</ul>
+				<ul class="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
+					<li><a class="nav-link" href="#"><img src="{{asset('images/user.svg')}}"></a></li>
+					<li><a class="btn btn-secondary me-2" href="{{ route('login') }}">Login</a></li>
+				</ul>
+			</div>
 		</div>
-	</div>
 
-</nav>
+	</nav>
